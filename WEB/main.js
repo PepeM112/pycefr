72846317f319a3acedd1a12afaba2720c9998f91
jmatas <j.matas.2019@alumnos.urjc.es>
2024-07-17 19:20:21 +0200

    Minor changes. Changed base path to allow plug-and-play

--- WEB/main.js
+++ WEB/main.js
@@ -1,141 +1,152 @@
 //-- PROGRAM TO CREATE A WEB PAGE
 
-//-- Import external module
-const fs = require('fs')
+//-- Import external modules
+const fs = require("fs");
+const path = require("path");
+
+//-- Define the base path as the root of the project directory
+const basePath = path.resolve(__dirname, "../");
 
 //-- Read html file
-const REPO = fs.readFileSync('repo.html', 'utf-8')
+const REPO = fs.readFileSync(path.join(__dirname, "repo.html"), "utf-8");
 
 //-- Read index.html
-let INDEX = fs.readFileSync('main.html', 'utf-8')
+let INDEX = fs.readFileSync(path.join(__dirname, "main.html"), "utf-8");
 
-//-- Name of the Json file to read
-const JSON_FILE = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/total_data.json")
-const JSON_FILESUM = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/summary_data.json")
-const JSON_FILEREPO = fs.readFileSync("/home/ana/Documentos/TFG/TFG/DATA_JSON/repo_data.json")
+//-- Define the path to the JSON files
+const jsonDir = path.join(basePath, "DATA_JSON");
+
+//-- Name of the Json files to read
+const JSON_FILE = fs.readFileSync(
+  path.join(jsonDir, "total_data.json"),
+  "utf-8"
+);
+const JSON_FILESUM = fs.readFileSync(
+  path.join(jsonDir, "summary_data.json"),
+  "utf-8"
+);
+const JSON_FILEREPO = fs.readFileSync(
+  path.join(jsonDir, "repo_data.json"),
+  "utf-8"
+);
 
 //-- Create the store structure from the contents of the file
 //-- Return us the json structure
 var data_total = JSON.parse(JSON_FILE);
-var data_summary = JSON.parse(JSON_FILESUM)
-var data_repo = JSON.parse(JSON_FILEREPO)
+var data_summary = JSON.parse(JSON_FILESUM);
+var data_repo = JSON.parse(JSON_FILEREPO);
 
 //-- Variable that is going to have all the buttons available
-let button = ''
+let button = "";
 
 //-- Get information
 //-- Get Repository name
-repository = Object.keys(data_total)
-
+const repository = Object.keys(data_total);
 
 //-- Create a button for each repository
-for (i=0; i<repository.length; i++){
-  button += "<button role='link'onclick=window.location='" +
-            repository[i] + ".html'>Repository " + repository[i]+
-            "</button><br><br>" + '\n'
+for (let i = 0; i < repository.length; i++) {
+  button +=
+    "<button role='link' onclick=window.location='" +
+    repository[i] +
+    ".html'>Repository " +
+    repository[i] +
+    "</button><br><br>" +
+    "\n";
 }
-INDEX = INDEX.replace('BUTTON', button)
-//-- Write total in new html file
-fs.writeFileSync('index.html', INDEX);
+INDEX = INDEX.replace("BUTTON", button);
 
+//-- Write total in new html file
+fs.writeFileSync(path.join(__dirname, "index.html"), INDEX);
 
 //-- Obtain information from each repository
-for (repo=0; repo<repository.length; repo++){
+for (let repo = 0; repo < repository.length; repo++) {
   //-- Assign the value of REPO
-  let content = REPO
+  let content = REPO;
   //-- Variable total
-  let total = ''
-  let name_repo = "<h2> REPOSITORY: " + repository[repo] + "</h2>" + '\n'
+  let total = "";
+  let name_repo = "<h2> REPOSITORY: " + repository[repo] + "</h2>" + "\n";
   //-- Get total content
-  content_total = data_total[repository[repo]]
+  let content_total = data_total[repository[repo]];
 
   //-- Get Files names
-  files = Object.keys(content_total)
+  let files = Object.keys(content_total);
 
-  for (file=0; file<files.length; file++){
+  for (let file = 0; file < files.length; file++) {
     //-- Get name
-    name_file = files[file]
-    total += '<h3>NAME FILE : ' + name_file + '<h3>'
-    let content_file = data_total[repository[repo]][name_file]
+    let name_file = files[file];
+    total += "<h3>NAME FILE : " + name_file + "<h3>";
+    let content_file = content_total[name_file];
 
     //-- Get levels
-    let levels = content_file["Levels"]
-    total += '<h4>LEVELS: <h4>' + '\n'
-    for (i=0; i<Object.keys(levels).length; i++){
-      keys = Object.keys(levels);
-      values = Object.values(levels);
-      total += ('<p>Levels ' + keys[i] + ': ' + values[i] + '</p>' + '\n');
+    let levels = content_file["Levels"];
+    total += "<h4>LEVELS: <h4>" + "\n";
+    for (let i = 0; i < Object.keys(levels).length; i++) {
+      let keys = Object.keys(levels);
+      let values = Object.values(levels);
+      total += "<p>Levels " + keys[i] + ": " + values[i] + "</p>" + "\n";
     }
     //-- Get classes
-    let clase = content_file["Class"]
-    total += '<h4>CLASSES: <h4> ' + '\n'
-    for (i=0; i<Object.keys(clase).length; i++){
-      keys = Object.keys(clase)
-      values = Object.values(clase)
-      total += ('<p>Class ' + keys[i] + ': ' + values[i] + '</p>' + '\n')
+    let clase = content_file["Class"];
+    total += "<h4>CLASSES: <h4> " + "\n";
+    for (let i = 0; i < Object.keys(clase).length; i++) {
+      let keys = Object.keys(clase);
+      let values = Object.values(clase);
+      total += "<p>Class " + keys[i] + ": " + values[i] + "</p>" + "\n";
     }
   }
-  total_repo = repo_summary();
-  content = content.replace('REPO', name_repo)
-  content = content.replace('TOTAL', total)
-  content = content.replace('SUMMARY', total_repo)
+  let total_repo = repo_summary(repo);
+  content = content.replace("REPO", name_repo);
+  content = content.replace("TOTAL", total);
+  content = content.replace("SUMMARY", total_repo);
 
-  name_html = (repository[repo] + '.html')
-
+  let name_html = path.join(__dirname, repository[repo] + ".html");
 
   //-- Write total in new html file
   fs.writeFileSync(name_html, content);
 }
 
-
 //-- Obtain repository summary information
-function repo_summary(){
+function repo_summary(repo) {
   //-- Variable with the summary of each repository
-  let total_repo = ('<h3>Summary of file analysis: <h3>' + '\n');
-  repos = Object.keys(data_repo);
+  let total_repo = "<h3>Summary of file analysis: <h3>" + "\n";
+  let repos = Object.keys(data_repo);
   //-- Get repository name
-  repo_name = repos[repo]
+  let repo_name = repos[repo];
   //-- Get content of each repository
-  content = data_repo[repo_name];
-  for(elem=0; elem<Object.keys(content).length; elem++){
-    keys = Object.keys(content);
-    values = Object.values(content);
-    if (elem==0){
-      total_repo += ('<h4>LEVELS: <h4>' + '\n');
-    }else{
-      total_repo += ('<h4>CLASSES: <h4>' + '\n');
+  let content = data_repo[repo_name];
+  for (let elem = 0; elem < Object.keys(content).length; elem++) {
+    let keys = Object.keys(content);
+    let values = Object.values(content);
+    if (elem === 0) {
+      total_repo += "<h4>LEVELS: <h4>" + "\n";
+    } else {
+      total_repo += "<h4>CLASSES: <h4>" + "\n";
     }
-    for (value=0; value<Object.keys(values[elem]).length; value++){
-      key = Object.keys(values[elem])[value];
-      val =Object.values(values[elem])[value];
-      total_repo += ('<p>' + keys[elem] + ' ' + key + ': ' + val +
-                     '</p>' + '\n');
+    for (let value = 0; value < Object.keys(values[elem]).length; value++) {
+      let key = Object.keys(values[elem])[value];
+      let val = Object.values(values[elem])[value];
+      total_repo += "<p>" + keys[elem] + " " + key + ": " + val + "</p>" + "\n";
     }
   }
-  console.log(total_repo)
-  return(total_repo);
-
+  return total_repo;
 }
-
-
 
 //-- Obtain summary information
-let total_summary = ''
-type = Object.keys(data_summary)
-for (i=0; i<type.length; i++){
-  key = type[i] //-- Levels or Class
-  total_summary += '<h4>' + key.toUpperCase() + ':<h4> ' + '\n'
-  content = data_summary[key]
-  for(elem=0; elem<Object.keys(content).length; elem++){
-    keys = Object.keys(content)
-    values = Object.values(content)
-    total_summary += ('<p>' + key + ' ' + keys[elem] + ': ' + values[elem] +
-                      '</p>' + '\n')
+let total_summary = "";
+let type = Object.keys(data_summary);
+for (let i = 0; i < type.length; i++) {
+  let key = type[i]; //-- Levels or Class
+  total_summary += "<h4>" + key.toUpperCase() + ":<h4> " + "\n";
+  let content = data_summary[key];
+  for (let elem = 0; elem < Object.keys(content).length; elem++) {
+    let keys = Object.keys(content);
+    let values = Object.values(content);
+    total_summary +=
+      "<p>" + key + " " + keys[elem] + ": " + values[elem] + "</p>" + "\n";
   }
 }
-INDEX = INDEX.replace('SUMMARY', total_summary)
+INDEX = INDEX.replace("SUMMARY", total_summary);
 //-- Write total in new html file
-fs.writeFileSync('index.html', INDEX);
-
+fs.writeFileSync(path.join(__dirname, "index.html"), INDEX);
 
 console.log("Website created.");
