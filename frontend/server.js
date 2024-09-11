8929e10ea9d87c329587f2921dcfd4ea0c6cba6f
jmatas <j.matas.2019@alumnos.urjc.es>
2024-09-11 12:20:51 +0200

    Changes to server

--- frontend/server.js
+++ frontend/server.js
@@ -11,13 +11,15 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
 
-// Middleware para parsear JSON en las solicitudes
 app.use(express.json());
 
-// Middleware para servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware para redirigir solicitudes sin la extensión ".html" a los archivos HTML correspondientes
+// Routing
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'summary.html'));
+});
+
 app.get('/:filename', (req, res, next) => {
   const { filename } = req.params;
   const filePath = path.join(__dirname, 'public', 'html', `${filename}.html`);
@@ -26,47 +28,41 @@ app.get('/:filename', (req, res, next) => {
     if (!err) {
       res.sendFile(filePath);
     } else {
-      next(); // Si el archivo no existe, pasa al siguiente middleware
+      next();
     }
   });
 });
 
-// Middleware para servir archivos CSS y otros archivos estáticos
+// Load CSS
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 
-// Endpoint para manejar la edición y ejecución del archivo de configuración
+// Run project
 app.post('/run-python', (req, res) => {
   const configFilePath = path.join(__dirname, 'config', 'config.json');
 
   // Guardar el archivo de configuración recibido
   fs.writeFile(configFilePath, JSON.stringify(req.body), (err) => {
     if (err) {
-      return res.status(500).send('Error al guardar el archivo de configuración');
+      return res.status(500).send(err);
     }
 
     // Ejecutar el script de Python
-    exec('python3 backend/pycerfl.py', (error, stdout, stderr) => {
+    exec('python3 pycerfl.py', (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error al ejecutar el script de Python: ${error.message}`);
+        console.error(error.message);
         return res.status(500).send('Error al ejecutar el script de Python');
       }
 
       if (stderr) {
-        console.error(`Error en el script de Python: ${stderr}`);
+        console.error(stderr);
         return res.status(500).send('Error en el script de Python');
       }
 
       console.log(`Resultado del script de Python: ${stdout}`);
       
-      // Enviar la respuesta al cliente
       res.send(stdout);
     });
   });
-});
-
-// Servir el archivo HTML principal
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'html', 'summary.html'));
 });
 
 app.listen(port, () => {
