5dfbac61450edb216705908c17eae3be1adea11b
PepeM112 <129164725+PepeM112@users.noreply.github.com>
2025-11-29 12:45:54 +0100

    Deleted old frontend

--- frontend/public/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.querySelector('.sidebar-toggle');
-    const sidebar = document.querySelector('.sidebar');
-
-    burgerMenu.addEventListener('click', () => {
-        sidebar.classList.toggle('hidden');
-    });
-});
