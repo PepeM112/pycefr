0f876f7a96ecb19b48f3210afe45c6ad52506b5f
PepeM112 <129164725+PepeM112@users.noreply.github.com>
2025-12-06 17:44:43 +0100

    first changes

---
+++ frontend-vue/src/components/LanguageSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-menu v-model="isMenuOpen" location="top" class="language-selector">
+    <template #activator="{ props }">
+      <v-btn
+        v-bind="props"
+        class="d-flex px-0 py-2"
+        density="compact"
+        variant="text"
+      >
+        <country-flag
+          :country="getLanguageFlag(selectedLanguage)"
+          style="transform: scale(0.35) translate(0, -25%)"
+        />
+      </v-btn>
+      <v-list v-if="isMenuOpen" class="rounded-lg">
+        <v-list-item
+          v-for="option in languageOptions"
+          :key="option.code"
+          style="cursor: pointer"
+          @click="changeLanguage(option.code)"
+        >
+          <country-flag
+            class="ml-1 mr-1"
+            :country="(option.flag || option.code)"
+            size="small"
+            style="transform: scale(0.4)"
+          />
+          {{ t(option.label) }}
+        </v-list-item>
+      </v-list>
+    </template>
+  </v-menu>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { locale, t } = useI18n();
+
+const selectedLanguage = ref<string>(locale.value);
+const isMenuOpen = ref(false);
+
+const languageOptions = [
+  { code: 'en', label: 'english', flag: 'gb' },
+  { code: 'es', label: 'spanish' },
+];
+
+function getLanguageFlag(code: string): string {
+  const option = languageOptions.find(opt => opt.code === code);
+  return option?.flag || code;
+}
+
+function changeLanguage(code: string) {
+  selectedLanguage.value = code;
+  locale.value = code;
+}
+</script>
+<style lang="scss">
+
+</style>
