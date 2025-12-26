bea8856b5d1194d273c922322bb11632d07c4459
PepeM112 <129164725+PepeM112@users.noreply.github.com>
2025-12-26 20:46:19 +0100

    PYCEFR-21 Deleted file

--- backend-fastapi/api/catalog_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from typing import List
-import db_utils
-from models.analysis import *
-from models.common import *
-
