048175fbdeeb6811371fa2f43caf04d352d97c80
PepeM112 <129164725+PepeM112@users.noreply.github.com>
2026-01-06 16:50:04 +0100

    PYCEFR-25 Changed folder name

--- pycerfl.py
+++ pycerfl.py
@@ -1,8 +1,8 @@
 import argparse
 import os
 
-from backend-fastapi.services.analyzer import console
-from backend-fastapi.services.analyzer.analyzer import (
+from backend.services.analyzer import console
+from backend.services.analyzer.analyzer import (
     request_url,
     run_directory,
     run_user,
