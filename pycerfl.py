9ff10b65749002d244174866ac9759f969dd6920
PepeM112 <129164725+PepeM112@users.noreply.github.com>
2026-01-06 16:44:49 +0100

    PYCEFR-25 Removed old backend

--- pycerfl.py
+++ pycerfl.py
@@ -1,14 +1,19 @@
-import os
 import argparse
-from backend.scripts.analyzer import request_url, run_directory, run_user
-from backend.scripts import console
+import os
+
+from backend-fastapi.services.analyzer import console
+from backend-fastapi.services.analyzer.analyzer import (
+    request_url,
+    run_directory,
+    run_user,
+)
 
 
-def list_results():
+def list_results() -> None:
     """
     List files in the ./results/ directory.
     """
-    results_dir = './results/'
+    results_dir = "./results/"
     try:
         files = os.listdir(results_dir)
         if not files:
@@ -21,20 +26,39 @@ def list_results():
         print(f"The directory {results_dir} does not exist.")
 
 
-def main():
+def main() -> None:
     """
     Main function to handle command-line arguments and invoke appropriate actions.
     """
-    parser = argparse.ArgumentParser(description="An utility for analyzing Python level in local and Github repositories.")
+    parser = argparse.ArgumentParser(
+        description="An utility for analyzing Python level in local and Github repositories."
+    )
     parser.add_argument("-d", "--directory", type=str, help="Run in directory mode")
-    parser.add_argument("-r", "--repo", type=str, help="Run with the specified repository URL")
-    parser.add_argument("-u", "--user", type=str, help="Run with the specified user identifier")
+    parser.add_argument(
+        "-r", "--repo", type=str, help="Run with the specified repository URL"
+    )
+    parser.add_argument(
+        "-u", "--user", type=str, help="Run with the specified user identifier"
+    )
     parser.add_argument("-c", "--console", type=str, help="See results in console")
-    parser.add_argument("-l", "--list", action='store_true', help="List available result files")
+    parser.add_argument(
+        "-l", "--list", action="store_true", help="List available result files"
+    )
 
     args = parser.parse_args()
 
-    if sum([bool(args.directory), bool(args.repo), bool(args.user), bool(args.console), bool(args.list)]) != 1:
+    if (
+        sum(
+            [
+                bool(args.directory),
+                bool(args.repo),
+                bool(args.user),
+                bool(args.console),
+                bool(args.list),
+            ]
+        )
+        != 1
+    ):
         parser.print_help()
     elif args.console:
         console.main(f"results/{args.console}")
@@ -46,6 +70,7 @@ def main():
         request_url(args.repo)
     elif args.user:
         run_user(args.user)
+
 
 if __name__ == "__main__":
     main()
